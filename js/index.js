643e59a509ebf1f59c153048e9df74c55b26522b
Iñaki Sigüenza <inakisiguenza@MacBook-Pro-de-Inaki.local>
2023-03-07 15:12:23 -0600

    iOS Lab Web

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
