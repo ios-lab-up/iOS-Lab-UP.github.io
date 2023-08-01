0571d7fc8b817d933f876dcf61c0032e000b2939
Iñaki Sigüenza <isiguenzan@gmail.com>
2023-08-01 14:06:06 -0600

    redirect

---
+++ js/redirect.js
@@ -0,0 +1,7 @@
+// Obtén el path de la URL
+var path = window.location.pathname;
+
+// Si el path comienza con "/0218982", redirige al usuario a la página de GitHub correspondiente
+if (path.startsWith("/0218982")) {
+    window.location.href = "https://github.com/isiguenza";
+}
