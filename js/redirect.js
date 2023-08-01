138b0e79e442af454c6a731e2e8d7397f9da9c68
Iñaki Sigüenza <isiguenzan@gmail.com>
2023-08-01 14:39:11 -0600

    redirect bug

--- js/redirect.js
+++ js/redirect.js
@@ -1,15 +1,16 @@
 var githubUsernames = {
     "0218982": "isiguenza",
-    "0250009": "luisced",
-   
+    "0350009": "luisced"
   };
   
-
+  
   var userId = window.location.pathname.split("/").pop();
   var githubUsername = githubUsernames[userId];
   if (githubUsername) {
     window.location.href = "https://github.com/" + githubUsername;
-  } else {
-    window.location.href = "/404.html";
+  } else {  
+    if (window.location.pathname !== "/404.html") {
+      window.location.href = "/404.html";
+    }
   }
   
