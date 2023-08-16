1ba6528d176d0c584011f898fa33eb9ac92717c1
Iñaki Sigüenza <isiguenzan@gmail.com>
2023-08-16 14:57:43 -0600

    new users

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,11 @@
 var githubUsernames = {
     "isiguenza": "isiguenza",
     "0250009": "luisced",
-    "ioslab" : "iOS-Lab-UP"
+    "ioslab" : "iOS-Lab-UP",
+    "0232174" : "mauu2001",
+    "0234636" : "marineehan-2307",
+    "0250964" : "detectov",
+    "0231672" : "ivancruzl",
   };
   
   
